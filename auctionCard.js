f1cd9261844325f9968ca9d2ec7ab9491f04b539
Vale-pixel <1005896964@u.icesi.edu.co>
2023-04-27 16:55:10 -0500

    Apply sass.

--- auctionCard.js
+++ auctionCard.js
@@ -1,5 +1,5 @@
 class auctionCard extends HTMLElement {
-    connectedCallback() {
+    connectedCallback(){
       this.getData();
     }
   
@@ -12,7 +12,14 @@ class auctionCard extends HTMLElement {
         console.error(error);
       }
     }
-  
+    /*
+    <div class="s-finished-auctions">
+    <div class="nft-img s"></div>
+        <img src="https://images.unsplash.com/photo-1645731504636-72725e46b26b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5mdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60" alt="finished-auction-img" class="f-auction">
+        <h1 class="nft-name">UNHCR hand</h1>
+        <p class="description">Okay beards</p>
+        <div class="ended">3.00 SOL ENDED</div>
+</div>*/
     renderCards(results) {
       //creo el elemento auction-container en html
       const cardContainer = document.createElement('div');
@@ -26,19 +33,26 @@ class auctionCard extends HTMLElement {
   
         //constante para imagen
         const image = document.createElement('img');
+        image.classList.add('card__nftImage')
         image.src = result.url;
   
         //creo el título del producto por el nombre del nft
-        const name = document.createElement('h2');
+        const name = document.createElement('h1');
+        name.classList.add('card__nftName')
         name.textContent = result.name;
   
+        const collection = document.createElement('h2');
+        collection.classList.add('card__collectionName')
+        collection.textContent = result.collection;
         //creo elemento para la firma de la colección
-        const collection = document.createElement('p');
-        collection.textContent = `Auction ended: ${result.collection}`;
+        const price = document.createElement('p');
+        price.classList.add('card__price')
+        price.textContent = `Ended: ${result.price}${" "}${result.cryptocurrency}`;
   
         card.appendChild(image);
         card.appendChild(name);
         card.appendChild(collection);
+        card.appendChild(price);
         cardContainer.appendChild(card);
       });
       this.appendChild(cardContainer);
